e2abcdbfff1a50ff26c2d678ebb0268f2f377417
Rituraj Raman <riturama@codeaurora.org>
2021-10-19 10:12:29 +0530

    msm: kgsl: Add condition to check for power flag
    adreno_power_stats() expects the caller would enable the clocks first
    and it doesn’t check for the clock status. Added flag bits to
    set/clear the clock status and checked the clock status before accessing
    the perfctr_pwr_lo register in adreno_power_stats().
    
    Change-Id: If3c00bf62ba8e9111485fa78f7528920653f4a80
    Signed-off-by: Rituraj Raman <riturama@codeaurora.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -1149,8 +1149,8 @@ static void kgsl_pwrctrl_clk(struct kgsl_device *device, int state,
 		return;
 
 	if (state == KGSL_PWRFLAGS_OFF) {
-		if (test_and_clear_bit(KGSL_PWRFLAGS_CLK_ON,
-			&pwr->power_flags)) {
+		if (test_bit(KGSL_PWRFLAGS_CLK_ON,
+						&pwr->power_flags)) {
 			trace_kgsl_clk(device, state,
 					kgsl_pwrctrl_active_freq(pwr));
 			/* Disable gpu-bimc-interface clocks */
@@ -1185,9 +1185,11 @@ static void kgsl_pwrctrl_clk(struct kgsl_device *device, int state,
 					pwr->num_pwrlevels - 1);
 			}
 		}
+
+		clear_bit(KGSL_PWRFLAGS_CLK_ON, &pwr->power_flags);
 	} else if (state == KGSL_PWRFLAGS_ON) {
-		if (!test_and_set_bit(KGSL_PWRFLAGS_CLK_ON,
-			&pwr->power_flags)) {
+		if (!test_bit(KGSL_PWRFLAGS_CLK_ON,
+				&pwr->power_flags)) {
 			trace_kgsl_clk(device, state,
 					kgsl_pwrctrl_active_freq(pwr));
 			if (pwr->pwrlevels[0].gpu_freq > 0) {
@@ -1218,8 +1220,9 @@ static void kgsl_pwrctrl_clk(struct kgsl_device *device, int state,
 
 			/* Turn on the IOMMU clocks */
 			kgsl_mmu_enable_clk(&device->mmu);
-		}
 
+			set_bit(KGSL_PWRFLAGS_CLK_ON, &pwr->power_flags);
+		}
 	}
 }
 
