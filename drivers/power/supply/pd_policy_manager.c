4bae03d1e933d47fb1cd64184ea55adeb5694f73
Sparxie <266169054+sparxie33@users.noreply.github.com>
2026-04-08 22:55:17 +0000

    drivers: power: Allow fast charge up to 43°C (High Risk)
    Change-Id: I7a6bcdf796f677b352dcf80e3a1e34154ad57a2a

--- drivers/power/supply/pd_policy_manager.c
+++ drivers/power/supply/pd_policy_manager.c
@@ -877,7 +877,7 @@ static int battery_sw_jeita(struct usbpd_pm *pdpm)
                         pdpm->bat_temp,  jeita_curr, pdpm->therm_curr, pdpm->pps_temp_flag, pd_auth, pdpm->bat_cycle, pdpm->batt_auth);
 
     // Thermal charge bypass
-    if (pdpm->bat_temp < 400) {
+    if (pdpm->bat_temp < 430) {
         pdpm->therm_curr = jeita_curr;
     }
 
