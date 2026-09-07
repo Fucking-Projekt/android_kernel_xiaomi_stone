dereference23/kernel_xiaomi_sm6375

firmware_loader: Add /vendor/firmware
From veux-r-oss

Change-Id: I896e3908ad2ae1bb83461d0320b319b8ccc20bb0
Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

drivers/base/firmware_loader/main.c
@@ -446,10 +446,14 @@ static int fw_decompress_xz(struct device *dev, struct fw_priv *fw_priv,
 static char fw_path_para[256];
 static const char * const fw_path[] = {
 	fw_path_para,
+#ifdef CONFIG_WT_QGKI
+	"/vendor/firmware",
+#else
 	"/lib/firmware/updates/" UTS_RELEASE,
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
 	"/lib/firmware"
+#endif
 };
 
 /*

Fucking-Projekt/android_kernel_xiaomi_stone

drivers/base/firmware_loader/main.c
@@ -457,10 +457,14 @@ static const char * const fw_path[] = {
 	fw_path_para[7],
 	fw_path_para[8],
 	fw_path_para[9],
+#ifdef CONFIG_QGKI
+	"/vendor/firmware",
+#else
 	"/lib/firmware/updates/" UTS_RELEASE,
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
 	"/lib/firmware"
+#endif
 };
 
 static char strpath[PATH_SIZE * CUSTOM_FW_PATH_COUNT];